f18091e3339455edf478a611366d883793209cca
fvb-friba <128368996+fvb-friba@users.noreply.github.com>
2025-04-09 13:58:34 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from app.api import soil, climate, value
+from app.api import soil, climate, value, bonitaet
 
 app = FastAPI()
 
 app.include_router(soil.router)
 app.include_router(climate.router)
 app.include_router(value.router)
+app.include_router(boenität.router)
